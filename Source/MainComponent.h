1ce215da76a3a1659115cdfbe839153b2fdde68d
Liam Lacey <liamlacey@hotmail.com>
2013-03-21 11:32:04 +0000

    Added licensing information to the headers of each file.

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -6,6 +6,28 @@
   ==============================================================================
 */
 
+//
+//  MainComponent.h
+//  Audio Library Installer
+//
+//  Created by Liam Meredith-Lacey on 12/03/2013.
+//  Copyright 2013 nu desine.
+//
+//  This file is part of Audio Library Installer.
+//
+//  Audio Library Installer is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License, version 2, 
+//  as published by the Free Software Foundation.
+//  
+//  Audio Library Installer is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 #ifndef __MAINCOMPONENT_H_B87CFFDB__
 #define __MAINCOMPONENT_H_B87CFFDB__
 
